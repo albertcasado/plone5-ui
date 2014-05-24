d3eae17361be70b0654d9c6b4ae77a1bf91eaf07
Albert <albertcelma@gmail.com>
2014-05-24 15:05:13 +0200

    Js

--- Toolbar_plone/js/main.js
+++ Toolbar_plone/js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
 
 $(document).ready(function(){
-	if ($(window).height() < "768"){
+	if ($(window).width() < "768"){//mobile
 		$( "#edit-zone .plone-toolbar-logo" ).click(function() {
 			if ($(this).hasClass("open")){
 				$( "#edit-zone" ).css("right", "-120px");
@@ -34,32 +34,29 @@ $(document).ready(function(){
 		});
 	}
 	else { //not mobile
-		if ($("#edit-zone").hasClass("expanded")){
-			$("body").css("left", "120px");
-		}
-		if (!$("#edit-zone").hasClass("compressed")){
-			$( "#edit-zone.plone-toolbar-left .plone-toolbar-logo" ).click(function() {
-				if ($("#edit-zone").hasClass("expanded")){
-					$( "#edit-zone" ).removeClass("expanded");
-					$( "body" ).css("left", "60px");
-					$( "#edit-zone nav li" ).removeClass("active");
-				} else {
-					$( "#edit-zone" ).addClass("expanded");
-					$( "body" ).css("left", "120px");
-					$( "#edit-zone nav li" ).removeClass("active");
-				}
-			});
-		}
+		$( "#edit-zone.plone-toolbar-left .plone-toolbar-logo" ).click(function() {
+			if ($("#edit-zone").hasClass("expanded")){
+				$( "#edit-zone" ).removeClass("expanded");
+				$( "#edit-zone nav li" ).removeClass("active");
+			} else {
+				$( "#edit-zone" ).addClass("expanded");
+				$( "#edit-zone nav li" ).removeClass("active");
+			}
+			if ($("#edit-zone").hasClass("expanded") && !$("#edit-zone").hasClass("compressed")){
+				$("body").css("left", "120px");
+			} else if (!$("#edit-zone").hasClass("expanded") && !$("#edit-zone").hasClass("compressed")) {
+				$("body").css("left", "60px");
+			}
+		});
+
 		$( "#edit-zone.plone-toolbar-left .plone-toolbar-logo" ).dblclick(function() {
 			if ($("#edit-zone").hasClass("compressed")){
 				$( "#edit-zone" ).removeClass("compressed");
-				$( "body" ).css("left", "0px");
 			} else {
 				$( "#edit-zone" ).addClass("compressed");
-				$( "body" ).css("left", "0");
 			}
+			$("body").css("left", "0px");
 		});
-		
 		$( "#edit-zone nav li" ).has( "a .plone-toolbar-caret" ).click(function() {
 			if ($(this).hasClass("active")) {
 				$( "#edit-zone nav li" ).removeClass("active");
